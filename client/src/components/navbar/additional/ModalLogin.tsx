b3a7ceab71d2641dd7d24175f1fc11f662e79421
Pavlo Kolinko <paulkolins@gmail.com>
2021-03-17 15:00:18 +0200

    Modal and axios

--- client/src/components/navbar/additional/ModalLogin.tsx
+++ client/src/components/navbar/additional/ModalLogin.tsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import { Modal, Button } from 'antd';
+import {Modal, Button, Form, Input} from 'antd';
 
 export const ModalLogin = () => {
     const [visible, setVisible] = useState(false);
@@ -33,7 +33,21 @@ export const ModalLogin = () => {
                 confirmLoading={confirmLoading}
                 onCancel={handleCancel}
             >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
 
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
             </Modal>
         </>
     );
